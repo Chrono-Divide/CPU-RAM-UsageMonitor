b251587cf132c2aa6db6f7df54434cd520138773
Chrono-Divide <lijaaf64@gmail.com>
2026-07-07 16:11:16 +0900

    Bump version to v1.0.1

--- CircleMonitorWPF/Properties/AssemblyInfo.cs
+++ CircleMonitorWPF/Properties/AssemblyInfo.cs
@@ -48,5 +48,6 @@ using System.Windows;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("v1.0.1")]
